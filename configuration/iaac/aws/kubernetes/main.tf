9413cea39434948c7f2e2fd5561b7cffa42f3e5d
Rahul Anand <rahulanand2006@gmail.com>
2023-11-26 07:23:08 -0500

    Updating Subnets and S3 Bucket Name

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -7,7 +7,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "mybucket" # Will be overridden from build
+    bucket = "terraform-backend-state-rahul" # Will be overridden from build
     key    = "path/to/my/key" # Will be overridden from build
     region = "us-east-1"
   }
@@ -32,7 +32,7 @@ module "in28minutes-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster"
   cluster_version = "1.14"
-  subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"] #CHANGE
+  subnets         = ["subnet-086cc6f648047d26a", "subnet-0f3a38f69778b05f7"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
 
@@ -42,8 +42,8 @@ module "in28minutes-cluster" {
     {
       instance_type = "t2.micro"
       max_capacity  = 5
-      desired_capacity = 3
-      min_capacity  = 3
+      desired_capacity = 2
+      min_capacity  = 2
     }
   ]
 }
