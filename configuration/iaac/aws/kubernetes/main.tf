4b24cff1766567458edacfd8a74bc25d71624328
Rahul Anand <rahulanand2006@gmail.com>
2023-11-27 14:09:51 -0500

    Removing Error

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -29,7 +29,7 @@ provider "kubernetes" {
 }
 
 module "in28minutes-cluster" {
-  source          = "terraform-aws-modules/eks/aws/"
+  source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster"
   cluster_version = "1.14"
   subnets         = ["subnet-086cc6f648047d26a", "subnet-0f3a38f69778b05f7"] #CHANGE
